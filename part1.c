564ada8dcc245fed95d2af2f509605cc0a525ea1
Ivan Kulesh <kulesh94@gmail.com>
2013-12-09 19:47:13 +0400

    new file:   commonmmap.h 	modified:   part1.c 	modified:   part2.c

--- part1.c
+++ part1.c
@@ -14,49 +14,19 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <sys/msg.h>
-
-#define DATESIZE 26
-#define TYPESIZE 18
-#define PERMSIZE 10
-#define KEYFILE "./part1.c"
-#define RESULTFILE "./ls.tmp"
-#define HELLO 1
-#define READY 2
-
-typedef struct struct_for_file_info {
-	off_t file_size;
-	char create_date [DATESIZE];
-	char last_change_date [DATESIZE];
-	char file_type [TYPESIZE];
-	char file_permissions [PERMSIZE];
-	size_t name_lenght;
-	char file_name_user_group [];
-} file_info;
+#include "./commonmmap.h"
 
 int count_of_alocated_pointers = 0;
 void** array_of_alocated_pointers = NULL;
 
 char** list_of_files = NULL;
 
-void add_pointer (void* new_pointer);
-
-void my_error (char* message);
-
-void free_all_pointers ();
 
 int get_list_of_files ( DIR* directory );
 
 void get_properties ( int number_of_files , file_info ** files_properties, char ** argv);
 
-void increase_sem (int sem_id);
-	
-void decrease_sem (int sem_id);
-
 char* get_permissions (mode_t file_mode);
-
-int get_sem_id (char key_mod);
-
-void* get_mmap (char* path , size_t file_size);
 
 int main (int argc, char** argv) {
 	DIR* destination_directory;
@@ -143,32 +113,6 @@ int main (int argc, char** argv) {
 	return 0;
 }
 
-void add_pointer (void* new_pointer) {
-	count_of_alocated_pointers ++;
-	if (array_of_alocated_pointers == NULL )
-		array_of_alocated_pointers = (void**) malloc (sizeof (void*) );
-	else 
-		array_of_alocated_pointers = (void**) realloc (array_of_alocated_pointers , count_of_alocated_pointers * sizeof (void*) );
-	array_of_alocated_pointers [count_of_alocated_pointers - 1] = new_pointer;
-	return;
-}
-
-void my_error (char* message) {
-	int i;
-	write (STDERR_FILENO, message, strlen (message));
-	for (i = 0; i < count_of_alocated_pointers; ++i)
-		free (array_of_alocated_pointers [i]);
-	free (array_of_alocated_pointers);
-	exit (-1);
-}
-
-void free_all_pointers () {
-	int i;
-	for (i = 0; i < count_of_alocated_pointers; ++i)
-		free (array_of_alocated_pointers [i]);
-	free (array_of_alocated_pointers);
-	return;
-}
 
 int get_list_of_files ( DIR* directory ) {
 	struct dirent * new_record = NULL;
@@ -270,24 +214,6 @@ void get_properties (int number_of_files , file_info ** files_properties, char *
 	free ( tmp_file_info );
 }
 
-void increase_sem (int sem_id) {
-	struct sembuf mybuf;	
-	mybuf.sem_op = 1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop (sem_id, &mybuf, 1) < 0)
-		my_error ("WTF?! Can not do an operation on semafor!\n\0");
-	return;
-}
-void decrease_sem (int sem_id) {
-	struct sembuf mybuf;
-	mybuf.sem_op = -1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop (sem_id, &mybuf, 1) < 0)
-		my_error ("WTF?! Can not do an operation on semafor!\n\0");
-	return;
-}
 
 char * get_permissions (mode_t file_mode) {
 	char* answer = NULL;
@@ -325,33 +251,3 @@ char * get_permissions (mode_t file_mode) {
 	return answer;
 }
 
-int get_sem_id (char key_mod) {
-	int sem_id;	
-	key_t sem_key;
-
-	sem_key = ftok (KEYFILE , key_mod);
-	if (sem_key < 0)
-		my_error ("FATALL ERROR! GENERAL FAILURE GETING KEY FOR SEMAFOR!\n\0");
-	sem_id = semget (sem_key , 1 , IPC_CREAT | 0666);
-	if (sem_id < 0)
-		my_error ("ERROR semget! Exeting...\n\0");
-	return sem_id;
-}
-
-void* get_mmap (char* path , size_t file_size) {
-	int file_id;
-	void* file_pointer = NULL;
-
-	file_id = open ( path,  O_RDWR | O_CREAT | O_TRUNC , 0666 );
-	if ( file_id < 0 )
-		my_error ("Can not open file!\n\0");
-	if ( ftruncate ( file_id , file_size ) < 0 )
-		my_error ("Eroor! Can not change file size!\n\0");
-	file_pointer = mmap ( NULL , file_size , PROT_READ | PROT_WRITE , MAP_SHARED , file_id , 0 );
-	if ( file_pointer == MAP_FAILED )
-		my_error ("Mapping failed!\n\0");
-	if ( close (file_id) < 0 )
-		my_error ("Error when closing file!\n\0");
-	return file_pointer;
-
-}
--- part2.c
+++ part2.c
@@ -14,41 +14,14 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <sys/msg.h>
+#include "commonmmap.h"
 
-#define DATESIZE 26
-#define TYPESIZE 18
-#define PERMSIZE 10
-#define KEYFILE "./part1.c"
-#define RESULTFILE "./ls.tmp"
-#define HELLO 1
-#define READY 2
 
-typedef struct struct_for_file_info {
-	off_t file_size;
-	char create_date [DATESIZE];
-	char last_change_date [DATESIZE];
-	char file_type [TYPESIZE];
-	char file_permissions [PERMSIZE];
-	size_t name_lenght;
-	char file_name_user_group [];
-} file_info;
 
 int count_of_alocated_pointers = 0;
 void** array_of_alocated_pointers = NULL;
 
-void add_pointer (void* new_pointer);
 
-void my_error (char* message);
-
-void free_all_pointers ();
-
-void increase_sem (int sem_id);
-	
-void decrease_sem (int sem_id);
-
-int get_sem_id (char key_mod);
-
-void* get_mmap (char* path , size_t file_size);
 
 int main () {
 	int  msg_id , i;
@@ -114,77 +87,4 @@ int main () {
 	return 0;
 }
 
-void add_pointer (void* new_pointer) {
-	count_of_alocated_pointers ++;
-	if (array_of_alocated_pointers == NULL )
-		array_of_alocated_pointers = (void**) malloc (sizeof (void*) );
-	else 
-		array_of_alocated_pointers = (void**) realloc (array_of_alocated_pointers , count_of_alocated_pointers * sizeof (void*) );
-	array_of_alocated_pointers [count_of_alocated_pointers - 1] = new_pointer;
-	return;
-}
 
-void my_error (char* message) {
-	int i;
-	write (STDERR_FILENO, message, strlen (message));
-	for (i = 0; i < count_of_alocated_pointers; ++i)
-		free (array_of_alocated_pointers [i]);
-	free (array_of_alocated_pointers);
-	exit (-1);
-}
-
-void free_all_pointers () {
-	int i;
-	for (i = 0; i < count_of_alocated_pointers; ++i)
-		free (array_of_alocated_pointers [i]);
-	free (array_of_alocated_pointers);
-	return;
-}
-
-void increase_sem (int sem_id) {
-	struct sembuf mybuf;	
-	mybuf.sem_op = 1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop (sem_id, &mybuf, 1) < 0)
-		my_error ("WTF?! Can not do an operation on semafor!\n\0");
-	return;
-}
-void decrease_sem (int sem_id) {
-	struct sembuf mybuf;
-	mybuf.sem_op = -1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if (semop (sem_id, &mybuf, 1) < 0)
-		my_error ("WTF?! Can not do an operation on semafor!\n\0");
-	return;
-}
-
-int get_sem_id (char key_mod) {
-	int sem_id;	
-	key_t sem_key;
-
-	sem_key = ftok (KEYFILE , key_mod);
-	if (sem_key < 0)
-		my_error ("FATALL ERROR! GENERAL FAILURE GETING KEY FOR SEMAFOR!\n\0");
-	sem_id = semget (sem_key , 1 , IPC_CREAT | 0666);
-	if (sem_id < 0)
-		my_error ("ERROR semget! Exeting...\n\0");
-	return sem_id;
-}
-
-void* get_mmap (char* path , size_t file_size) {
-	int file_id;
-	void* file_pointer = NULL;
-
-	file_id = open ( path,  O_RDWR , 0666 );
-	if ( file_id < 0 )
-		my_error ("Can not open file!\n\0");
-	file_pointer = mmap ( NULL , file_size , PROT_READ | PROT_WRITE , MAP_SHARED , file_id , 0 );
-	if ( file_pointer == MAP_FAILED )
-		my_error ("Mapping failed!\n\0");
-	if ( close (file_id) < 0 )
-		my_error ("Error when closing file!\n\0");
-	return file_pointer;
-
-}
